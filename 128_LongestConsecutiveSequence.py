4615951a83a097045f2de33a040618f9d7d3acc5
Quoc Dinh <132872972+Fordinh4@users.noreply.github.com>
2023-06-29 15:20:16 +0700

    Leetcode Attempt

---
+++ 128_LongestConsecutiveSequence.py
@@ -0,0 +1,42 @@
+def longestConsecutive(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_count = 0
+        count_list = []
+        flag = False
+        temp_nums = sorted(set(nums))
+
+        for i in range(len(temp_nums) - 1):
+            if i == len(temp_nums) - 2:
+                  flag = True
+
+            if temp_nums[i+1] - temp_nums[i] == 1:
+                  max_count += 1
+            else:
+                  # Append the current longest sequence and reset the count
+                  count_list.append(max_count + 1)
+                  max_count = 0
+
+        if flag:
+            # To count the last number in the list nums
+            count_list.append(max_count + 1)
+
+        try:
+            return max(count_list)
+        
+        except:
+             if nums == []:
+                return 0
+             else:
+                return 1
+
+
+
+
+
+
+nums = [0,3,7,2,5,8,4,6,0,1]
+a = longestConsecutive(nums)
+print(a)
