04a9e9da6e66d622fae926530622a07c3140f67f
Quoc Dinh <132872972+Fordinh4@users.noreply.github.com>
2023-07-03 18:27:17 +0700

    Updated solution

--- Arrays_and_Hashing/1_TwoSum.py
+++ Arrays_and_Hashing/1_TwoSum.py
@@ -9,14 +9,24 @@ def twoSum(nums, target):
     #         if nums[index] + nums[count] == target:
     #             return [index, count]
 
-    # This one is O(n^2) because of two operations in line 15 (each operation is O(n) so it add up)
-    for index, num in enumerate(nums):
-        res = target - num
-        if res in nums and nums.index(res) != index:
-            return [index, nums.index(res)]
+    # # This one is O(n^2) because of two operations in line 15 (each operation is O(n) so it add up)
+    # for index, num in enumerate(nums):
+    #     res = target - num
+    #     if res in nums and nums.index(res) != index:
+    #         return [index, nums.index(res)]
 
+    # This one will be O(n):
+    Prevmap = {} # The format will be value : index
 
-
+    for i, n in enumerate(nums):
+        # Because sum is a + b so if we want to find the value of b we can use sum - value of a
+        res = target - n
+        
+        if res in Prevmap: 
+            # Check if b is already in the dict yet
+            return [Prevmap[res], i]
+        # If not, add it in the dict
+        Prevmap[n] = i
 
 
 
